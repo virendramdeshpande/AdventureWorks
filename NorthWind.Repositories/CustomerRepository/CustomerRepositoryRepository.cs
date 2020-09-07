81970061076e407d1f6d9fbb47c4854247c2e761
Virendramdeshpande <virendramdeshpande@gmail.com>
2020-09-07 10:58:05 +0530

    missing files have been added.

---
+++ NorthWind.Repositories/CustomerRepository/CustomerRepositoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthWind.Repositories.AdventureWorks;
+using NorthWind.Repositories.CustomerRepository;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.InsurenceContractRepository
+{
+    public class CustomerRepositoryRepository : ICustomerRepository
+    {
+        private AdventureContext _AdventureContext;
+
+        public async IAsyncEnumerable<Customer> GetAll()
+        {
+            _AdventureContext = new AdventureContext();
+           
+            await foreach (var item in _AdventureContext.Customer)
+                yield return item;
+        }
+    }
+}
---
+++ NorthWind.Repositories/CustomerRepository/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using NorthWind.Repositories.AdventureWorks;
+using System.Collections.Generic;
+
+
+namespace NorthWind.Repositories.CustomerRepository
+{
+    public interface ICustomerRepository
+    {
+        IAsyncEnumerable<Customer> GetAll();
+        
+    }
+}
